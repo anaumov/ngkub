334cd96074d19c3720ce5b03dfdaeaee81b0b737
Alex Naumov <alexsnaumov@gmail.com>
2012-04-07 11:56:47 +0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ngkub::Application.config.secret_token = 'bdfb9fd49161bd3a92d4ca5e6bf2aeafb62e56a61a9a2c73029a7fb9a77cd33d7c040e6345795a9c972263bc09a072df56cc71ede35fdbf78318fc554b7dae1c'
