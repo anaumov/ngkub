753f68c082d582a33f6a0f871efaa66b7562149d
alex naumov <alexsnaumov@gmail.com>
2012-04-10 21:55:22 +0400

    Небольшие правки в контроллере Category и тесте контроллера

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -1,21 +1,21 @@
 class CategoryController < ApplicationController
 
   def new
-    @cat = Category.new(params[:category])
+    cat = Category.new(params[:category])
+    cat.save
   end
   
   def show
-    @cat = Category.find(params[:category][:id])    
+    cat = Category.find(params[:category][:id])    
   end
 
   def delete
-    @cat = Category.find(params[:category][:id])
-    @cat.destroy    
+    cat = Category.find(params[:category][:id])
+    cat.destroy    
   end
 
   def edit
-    @cat = Category.find(params[:category][:id])
-
+    cat = Category.find(params[:category][:id])
   end
   
 end
--- spec/controllers/category_controller_spec.rb
+++ spec/controllers/category_controller_spec.rb
@@ -2,13 +2,14 @@ require 'spec_helper'
 
 describe CategoryController do
 
-  it "should create a Category" do
-    Category.should_receive(:new).with("title" => "sports", "slug" => "sport", "intro" => "all about sport")
+  it "should save a Category in db" do
+    Category.should_receive(:save)
     post :new, :category => {:title => "sports", :slug => "sport", :intro => "all about sport" }
   end
 
   it "should return a Category" do
     Category.should_receive(:show).with("id" => "1")
+    post :new, :category => {:title => "sports", :slug => "sport", :intro => "all about sport" }
     post :show, :category => {:id => 1 }
   end
 
