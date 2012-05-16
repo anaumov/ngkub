c8ab9d3dd8520719ad982c4430d1340df1373184
alex naumov <alexsnaumov@gmail.com>
2012-05-16 15:28:18 +0400

    Реализованы title и description для всех страниц show

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def title(page_title)
+    content_for(:title) { page_title }
+  end
+  
+  def meta_description(meta_descr)
+    content_for(:meta_descr) { meta_descr }
+  end
+
 end
