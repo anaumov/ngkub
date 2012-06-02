7ea3bf0d119e8da40ccb81a358baa3bb12260502
alex naumov <alexsnaumov@gmail.com>
2012-06-02 19:31:44 +0400

    Правка в модели для картинки

--- app/models/publication.rb
+++ app/models/publication.rb
@@ -3,7 +3,7 @@ class Publication < ActiveRecord::Base
   attr_accessible :title, :body, :slug, :intro, :category, :category_id, :newspic, :onmain, :created_at, 
                   :publish, :old_id, :issue
 
-  has_attached_file :newspic, :styles => { :medium => "537x358#", :thumb => "185x125"}, :default_url => "/assets/missing.png"
+  has_attached_file :newspic, :styles => { :medium => "537x358#", :thumb => "185x125"}, :default_url => "/assets/missing.png" 
 
   
   validates :title, :body, :presence => true
