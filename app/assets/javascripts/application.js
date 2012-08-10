bb16594dd2413c1637bfb91e30839685db7a0099
alex naumov <alexsnaumov@gmail.com>
2012-08-10 14:25:03 +0400

    Реализовано добавление комментариев через ajax

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,20 +12,36 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require twitter/bootstrap
 //= require_tree .
-
-
-	$(function(){
-		$('#slides').slides({
+//= require_self
+//= require twitter/bootstrap
+$(document).ready(function(){
+  $('#slides').slides({
 			preload: true,
       container: 'slider',
       paginationClass: 'pic_navi',
       play: 5000,
-	  });
-	});
+	  })
 
   $("#ans_now").click(function () {
     $("div.answer").show("slow");
-  });
+  })
+
+  $("span.add_comment").click(function(){
+    $(this).toggleClass('input');
+    $("div.new_comment").slideToggle('slow');    
+  })
+  
+  $('span.add_comment_bottom').click(function(){
+    var current = $('a.back_lnk').offset().top;
+   
+    $('html').animate({
+      scrollTop: '+=' + current + 'px'
+    }, 1200, function() {
+      $("div.new_comment").slideDown('slow');
+      $('span.add_comment').addClass('input');
+    });    
+    
+  })
+})
 
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,17 +6,20 @@ class CommentsController < ApplicationController
     if simple_captcha_valid?
       @comment = Comment.create(params[:comment])
       if @comment.errors.empty?
-        flash[:notice] = t(:comment_created)
+        @message = t(:comment_created)
       else
-        flash[:notice] = "Smth went wrong"
+        @message = "Smth went wrong"
       end
-      redirect_to @comment.commentable
-    else
+   else
       model = params[:comment][:commentable_type]
       object = model.constantize.find(params[:comment][:commentable_id])
-      flash[:alert] = t(:capcha_denied)
-      redirect_to object     
+      @message = t(:capcha_denied)
     end
+
+    respond_to do |format|
+      format.js 
+     end
+    
 
   end
 
