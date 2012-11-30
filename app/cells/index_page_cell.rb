309dc43ffd3eb5be74388af57bfec1e1a5f11de1
Alex Naumov <alexsnaumov@gmail.com>
2012-11-30 12:26:38 +0700

    интервью вынесены в cells

---
+++ app/cells/index_page_cell.rb
@@ -0,0 +1,33 @@
+class IndexPageCell < Cell::Rails
+
+  def interviews
+    @index_interviews = Interview.index
+    render
+  end
+
+  def comments
+    @index_comments = Comment.get_lastest
+    render
+  end
+
+  def interviews
+    @index_interviews = Interview.index
+    render
+  end
+
+  def interviews
+    @index_interviews = Interview.index
+    render
+  end
+
+  def interviews
+    @index_interviews = Interview.index
+    render
+  end
+
+  def interviews
+    @index_interviews = Interview.index
+    render
+  end
+
+end
